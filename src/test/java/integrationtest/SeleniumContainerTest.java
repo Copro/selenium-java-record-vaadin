3944edb4e0e98faebc4f8c402f70a74f641a86a4
Michael Benz <michaelbenz@gmx.net>
2022-04-27 02:38:24 +0200

    Stopping in @AfterAll

--- src/test/java/integrationtest/SeleniumContainerTest.java
+++ src/test/java/integrationtest/SeleniumContainerTest.java
@@ -5,15 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 
+import org.junit.Rule;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 /**
  * Simple example of plain Selenium usage skipping recording and using the Chrome browser.
@@ -21,41 +20,34 @@ import org.testcontainers.junit.jupiter.Testcontainers;
  * @author Copro
  *
  */
-@Testcontainers
 public class SeleniumContainerTest {
 
 	private final static String NETCUP_FORUM_URL = "https://forum.netcup.de/";
 	private final static String SEARCH_BUTTON_ID = "userPanelSearchButton";
 	private final static String SEARCH_INPUT_ID = "pageHeaderSearchInput";
 
-	@Container
-	@SuppressWarnings("rcawtypes")
-	private static final BrowserWebDriverContainer BROWSER_CONTAINER = new BrowserWebDriverContainer()
+	@Rule
+	@SuppressWarnings("rawtypes")
+	public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
 			.withCapabilities(new ChromeOptions())
 			.withRecordingMode(VncRecordingMode.RECORD_ALL, new File("target"));
-
-	private static RemoteWebDriver browser;
-
-	@BeforeAll
-	static void configureBrowser() {
-		browser = BROWSER_CONTAINER.getWebDriver();
-	}
 
 	/**
 	 * Simple test opening the netcup Forum and searching for 'Selenium'.
 	 */
 	@Test
 	public void simplePlainSeleniumTest() throws InterruptedException {
+		RemoteWebDriver driver = chrome.getWebDriver();
 
 		System.out.println("Start Chrome and open URL: " + NETCUP_FORUM_URL);
-		browser.get(NETCUP_FORUM_URL);
+		driver.get(NETCUP_FORUM_URL);
 
 		System.out.println("Click the Search button with ID: " + SEARCH_BUTTON_ID);
-		WebElement searchButton = browser.findElementById(SEARCH_BUTTON_ID);
+		WebElement searchButton = driver.findElementById(SEARCH_BUTTON_ID);
 		searchButton.click();
 
 		System.out.println("Fill in the search input element: " + SEARCH_INPUT_ID);
-		WebElement searchInput = browser.findElementById(SEARCH_INPUT_ID);
+		WebElement searchInput = driver.findElementById(SEARCH_INPUT_ID);
 		searchInput.sendKeys("Selenium");
 		searchInput.submit();
 
@@ -63,11 +55,14 @@ public class SeleniumContainerTest {
 		Thread.sleep(200);
 
 		System.out.println("Verifying that the Thread 'Windows auf VPS 200 ?' shows up in the search results.");
-		assertThat(browser.getPageSource())
+		assertThat(driver.getPageSource())
 				.as("The Thread 'Windows auf VPS 200 ?' shows up in the search results.")
 				.contains("Windows auf VPS 200 ?");
+	}
 
-		System.out.println("End. We are Done here and quitting WebDriver / Browser");
-		browser.quit();
+	@AfterAll
+	public void tearDown() {
+		System.out.println("End. We are Done here and quitting WebDriver by stopping container");
+		chrome.stop();
 	}
 }
